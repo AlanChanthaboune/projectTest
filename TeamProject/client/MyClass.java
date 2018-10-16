cf2a0f6bdaabb1bbc21e24998a5e9be3dc778f68
AlanChanthaboune <32425554+AlanChanthaboune@users.noreply.github.com>
2018-10-16 17:58:40 -0500

    Testing

--- TeamProject/client/MyClass.java
+++ TeamProject/client/MyClass.java
@@ -6,10 +6,11 @@ public class MyClass
   public static void main(String[] args)
   {
     //Print statements
-    System.out.println("Hello Joe");
+    System.out.println("Hello Bobby");
     
     System.out.println("Another print statement 2");
     System.out.println("Hello World");
+    System.out.println("This is a test!");
 
   }
 
