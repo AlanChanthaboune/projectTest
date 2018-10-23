b39626700de2909aeb4336d874e0e24e4468aeb3
AlanChanthaboune <32425554+AlanChanthaboune@users.noreply.github.com>
2018-10-22 23:50:04 -0500

    Add substringPractice

---
+++ TeamProject/client/SubstringPractice.java
@@ -0,0 +1,24 @@
+package client;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SubstringPractice {
+
+    public static void main(String[] args) {
+    	 Scanner in = new Scanner(System.in);
+    	 System.out.println("Enter String:");
+         String S = in.next();
+         System.out.println("Enter start:");
+         int start = in.nextInt();
+         System.out.println("Enter end:");
+         int end = in.nextInt();
+       
+         System.out.println(S.substring(start,end));
+         
+        
+    }
+}
